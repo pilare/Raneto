8415aafe239a3c2e480f3c41d3644320ad9a1bde
Marcin Pilarczyk <marcin.pilarczyk@uc.edu>
2018-08-24 15:30:37 -0400

    added config to login_page route so it can access config.base_url

--- app/routes/login_page.route.js
+++ app/routes/login_page.route.js
@@ -6,6 +6,7 @@ function route_login_page (config) {
 
     return res.render('login', {
       layout      : null,
+      config      : config,
       lang        : config.lang,
       rtl_layout  : config.rtl_layout,
       googleoauth : config.googleoauth
